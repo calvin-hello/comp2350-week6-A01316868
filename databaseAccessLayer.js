4c3c890875932c90527994e2e38d4c3c0a9549d4
Calvin Mwin-Era <45calvin45@gmail.com>
2026-02-19 00:58:39 -0800

    initial commit

---
+++ databaseAccessLayer.js
@@ -0,0 +1,22 @@
+const database = include('/databaseConnection');
+
+
+async function getAllUsers() {
+	let sqlQuery = `
+		SELECT * FROM web_user;
+	`;
+	
+	try {
+		const results = await database.query(sqlQuery);
+		console.log(results[0]);
+		return results[0];
+	}
+	catch (err) {
+		console.log("Error selecting from user table");
+		console.log(err);
+		return null;
+	}
+}
+
+
+module.exports = {getAllUsers}
